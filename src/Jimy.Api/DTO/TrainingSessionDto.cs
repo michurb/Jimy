b371c902642b3a964de7da6aa322cebb2a8ffb7d
michurb <urbanczyk.michal11@gmail.com>
2024-03-25 14:16:51 +0100

    blazor init

--- src/Jimy.Api/DTO/TrainingSessionDto.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Jimy.Api.DTO;
-
-public class TrainingSessionDto
-{
-    public int Id { get; set; }
-    public string Name { get; set; }
-    public List<ExerciseDetailsDto> ExercisesDetails { get; set; }
-}
