1c4240cb39d210fe4af1a69a8050f27e6ac9bd51
Michal <urbanczyk.michal11@gmail.com>
2024-06-14 10:42:18 +0200

    less go

--- src/Jimy.Shared/Domain/Exercise.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Jimmy.Shared.Domain;
-
-public class Exercise
-{
-    public int Id { get; set; }
-    public string Name { get; set; }
-    public string Description { get; set; }
-}
--- src/Jimy.Shared/Domain/WeightTrainingExercise.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Jimmy.Shared.Domain;
-
-public class WeightTrainingExercise : Exercise
-{
-    public int Sets { get; set; }
-    public int Reps { get; set; }
-}
