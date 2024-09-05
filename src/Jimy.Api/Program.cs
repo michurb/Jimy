b662746b093ae8873d4e364749707f95e7343f33
Michal <urbanczyk.michal11@gmail.com>
2024-09-05 15:11:23 +0200

    add swagger info

--- src/Jimy.Api/Program.cs
+++ src/Jimy.Api/Program.cs
@@ -1,15 +1,29 @@
+using Jimy.Api.Middleware;
+using Jimy.Business;
 using Jimy.Data.Data;
 using Jimy.Data.Interfaces;
 using Jimy.Data.Repositories;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddControllers();
 
+builder.Services.AddSwaggerGen(swagger =>
+{
+    swagger.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Title = "Jimy API",
+        Version = "v1"
+    });
+});
+
+
 builder.Services.AddDbContext<JimyDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+builder.Services.AddApplication();
 
 // Register repositories
 builder.Services.AddScoped<IUserRepository, UserRepository>();
@@ -25,6 +39,7 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
+app.UseMiddleware<ErrorHandlingMiddleware>();
 app.UseHttpsRedirection();
 app.MapControllers();
 
